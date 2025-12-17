1cee670d9aa00c64ba966949a536d10a0a2dabfd
T0chi <tochiidesign@gmail.com>
2025-12-17 05:33:37 +0100

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,23 @@ const eslintConfig = [
     {
         ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
         rules: {
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-assign-module-variable': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             '@typescript-eslint/no-require-imports': 'off',
+            '@typescript-eslint/no-undef': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            '@typescript-eslint/no-extra-semi': 'off',
+            '@typescript-eslint/no-magic-numbers': 'off',
+            '@typescript-eslint/no-restricted-syntax': 'off',
+            '@typescript-eslint/no-use-before-define': 'off',
+            '@typescript-eslint/no-var': 'off',
+            '@typescript-eslint/prefer-const': 'off',
+            '@typescript-eslint/prefer-destructuring': 'off',
+            '@typescript-eslint/prefer-object-spread': 'off',
+            '@typescript-eslint/prefer-reduce': 'off',
+            '@typescript-eslint/prefer-template': 'off',
         },
     },
 ];
